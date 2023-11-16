987d030d3f4f44cab6947dee07c52969d30e3b4b
Yoni Lavi <yoni@lavi.fm>
2023-11-16 17:54:58 +0000

    Minor formatting for Unit2

--- Unit2/partB/django_ucd_project/blog/views.py
+++ Unit2/partB/django_ucd_project/blog/views.py
@@ -1,44 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-posts = [
+# Next week, we will start introduce the Django database settings and ORM.
+# But for now, we'll just use a list of dictionaries to represent our blog posts.
+POSTS = [
     {
-        'author': 'Steve K',
-        'title': 'First Blog Post',
-        'content': 'Content for the first post',
-        'date_posted': '16/09/2023'
+        "author": "Steve K",
+        "title": "First Blog Post",
+        "content": "Content for the first post",
+        "date_posted": "16/09/2023",
     },
-
     {
-        'author': 'John K',
-        'title': 'Second Blog Post',
-        'content': 'Content for the second post',
-        'date_posted': '18/09/2023'
+        "author": "John K",
+        "title": "Second Blog Post",
+        "content": "Content for the second post",
+        "date_posted": "18/09/2023",
     },
-
     {
-        'author': 'Jane K',
-        'title': 'Third Blog Post',
-        'content': 'Content for the third post',
-        'date_posted': '19/09/2023'
+        "author": "Jane K",
+        "title": "Third Blog Post",
+        "content": "Content for the third post",
+        "date_posted": "19/09/2023",
     },
-
     {
-        'author': 'Batman',
-        'title': 'Fourth Blog Post',
-        'content': 'Content for the fourth post',
-        'date_posted': '19/09/2023'
+        "author": "Batman",
+        "title": "Fourth Blog Post",
+        "content": "Content for the fourth post",
+        "date_posted": "19/09/2023",
     },
-
 ]
 
-# Create your views here.
+
 def home(request):
-    context = {
-        'posts':posts
-    }
-    return render(request, 'blog/home.html', context)
+    context = {"posts": POSTS}
+    return render(request, "blog/home.html", context)
+
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
-   
+    return render(request, "blog/about.html", {"title": "About"})
