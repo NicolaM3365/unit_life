5101c19ce9edca0fc971b43f083986566ee25f5a
NicolaM3365 <128541270+NicolaM3365@users.noreply.github.com>
2023-11-28 21:58:53 +0000

    initial commit

--- Unit2/partA/django_ucd_project/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse(b'<h1>Blog Home</h1>')
-
-
-def about(request):
-    return HttpResponse(b'<h1>Blog About</h1>')
---
+++ Unit_life/life_project/life_blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+# posts = [
+#     {
+#         'author': 'Steve K',
+#         'title': 'First Blog Post',
+#         'content': 'Content for the first post.',
+#         'date_posted': '12/07/2023'
+#     },
+
+#      {
+#         'author': 'Scott K',
+#         'title': 'Second Blog Post',
+#         'content': 'Content for the second post.',
+#         'date_posted': '13/07/2023'
+#     }
+
+# ]
+
+# Create your views here.x
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'home.html', context)
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About'})
