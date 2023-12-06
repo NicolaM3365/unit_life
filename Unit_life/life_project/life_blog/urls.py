1a5b4bd6d0638499c9bbf10feb3b5c1cb316fbc1
NicolaM3365 <128541270+NicolaM3365@users.noreply.github.com>
2023-12-06 22:03:17 +0000

    unit_7

--- Unit_life/life_project/life_blog/urls.py
+++ Unit_life/life_project/life_blog/urls.py
@@ -5,7 +5,6 @@ from .import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='blog-home'),
     path('about/',views.about, name='blog-about'),
 ]
--- Unit_life/life_project/life_project/settings.py
+++ Unit_life/life_project/life_project/settings.py
@@ -122,9 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'life_blog/static',
 ]
 
+MEDIA_ROOT = BASE_DIR / 'media' # Newer modern and Pythonic way
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
