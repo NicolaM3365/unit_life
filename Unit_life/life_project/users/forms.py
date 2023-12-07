5263ccbca6361889306ce2781d5757b496834b08
NicolaM3365 <128541270+NicolaM3365@users.noreply.github.com>
2023-12-07 21:47:07 +0000

    unit8_1

--- Unit_life/life_project/users/forms.py
+++ Unit_life/life_project/users/forms.py
@@ -3,6 +3,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
@@ -21,3 +22,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
