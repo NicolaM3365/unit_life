3571a0fb912d29f40f36d30f70fbed04389b995e
NicolaM3365 <128541270+NicolaM3365@users.noreply.github.com>
2024-01-06 18:32:36 +0000

    unit_11

--- Unit_life/life_project/life_project/settings.py
+++ Unit_life/life_project/life_project/settings.py
@@ -148,3 +148,11 @@ CRISPY_FAIL_SILENTLY = not DEBUG
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER'] # Login email address, Stored as an environment varaible
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD'] # App Password set from gmail, Stored as an environment varaible
+
